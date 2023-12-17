8e148e294bfa019b33fb8c9d63ee6fc16da32969
Kalok <kalokyeep@gmail.com>
2023-12-17 21:50:18 +0800

    first

---
+++ app/src/main/java/com/example/wia2007mad/ELearning/ThirdFragment.java
@@ -0,0 +1,46 @@
+package com.example.wia2007mad.ELearning;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wia2007mad.R;
+import com.example.wia2007mad.databinding.FragmentFirstBinding;
+import com.example.wia2007mad.databinding.FragmentThirdBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ThirdFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ThirdFragment extends Fragment {
+
+    private FragmentThirdBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentThirdBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
