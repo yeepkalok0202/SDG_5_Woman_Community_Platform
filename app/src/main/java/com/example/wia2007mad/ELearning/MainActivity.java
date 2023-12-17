9eed154289e7f8fe599187a4718e4f1404a919a4
Kalok <kalokyeep@gmail.com>
2023-12-18 02:01:06 +0800

    first

---
+++ app/src/main/java/com/example/wia2007mad/ELearning/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.wia2007mad.ELearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wia2007mad.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
